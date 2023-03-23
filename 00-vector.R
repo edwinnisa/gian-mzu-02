a6ce8c49ca9c17650985de237851a874a82bba3d
edwinnisa <edwinnisa1996@gmail.com>
2023-03-23 15:32:59 +0530

    kimi

--- 00-vector.R
+++ 00-vector.R
@@ -14,3 +14,11 @@ y = c (8, 1, 100)
 # data types
 # vector
 # string split functions
+surname = c ()
+surname[1] = strsplit(many_names[1], split= " ") [[1]][2]
+surname[2] = strsplit(many_names[2], split= " ") [[1]][2]
+surname[3] = strsplit(many_names[3], split= " ") [[1]][2]
+sn = c()
+for(i in 1:3) {
+	 sn [i] = strsplit(many_names[i], split = " ") [[1]][2]
+}
